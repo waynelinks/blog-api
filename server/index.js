24b4d060b5e1ee0821ac45f9f4d6c0b2927cbfd4
Wayne Links <wayne.links@gmail.com>
2019-07-17 05:05:22 +0200

    setup server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const { app, winston, config } = require('../app')
+
+
+const port = process.env.PORT || config.get('PORT')
+const server = http.createServer(app)
+
+server.listen(port, (err) => {
+  if (err) {
+    winston.exception(`${err.message}`)
+  }
+
+  winston.info(`> App running on port ${port}`)
+})
