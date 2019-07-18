07320fc49dc6422b19f20804d690f5576e29c9ae
Wayne Links <wayne.links@gmail.com>
2019-07-18 02:11:06 +0200

    set useCreateIndex flag to true on db connection

--- startup/db.js
+++ startup/db.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose')
 module.exports = (winston, config) => {
   const db = config.get('DB.host')
   mongoose
-    .connect(db, { useNewUrlParser: true, useFindAndModify: false })
+    .connect(db, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true })
     .then(() => winston.info(`> Connected to ${db}...`))
 }
